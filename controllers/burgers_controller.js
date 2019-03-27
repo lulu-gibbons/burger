1255b18bafe7bd03b11ec9537f1a06121ec8b01d
Lacey Gibbons <lazyrider@Laceys-MacBook-Pro.local>
2019-03-26 22:27:54 -0700

    database created, basic files created to return hardcoded database info

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var router = express.Router();
+var burger = require('../models/burger.js');
+
+// Import the model (burger.js) to use its database functions.
+//var cat = require("../models/burgers.js");
+
+// ALL ROUTES
+// =============================================================
+
+//base route
+router.get('/', function(req, res) {
+  //calling burger.all from burger.js file
+  //passing burger_data into the function
+  burger.all(function(burger_data){
+    console.log(burger_data);
+    res.render("index");
+  })
+});
+
+
+// Export routes for server.js to use.
+module.exports = router;
